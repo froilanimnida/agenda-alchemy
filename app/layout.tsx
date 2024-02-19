8cc8da86b1556f936dc671ffb279564b39bf262e
froilaaaaan1 <froilanaquino1@gmail.com>
2024-02-19 11:13:52 +0800

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <main>
       <body className={inter.className}>{children}</body>
+      </main>
     </html>
-  );
+  )
 }
