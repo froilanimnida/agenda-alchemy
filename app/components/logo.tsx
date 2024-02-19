5c5353fcae896fa3faac67abb533c22e1bd8f9b5
froilaaaaan1 <froilanaquino1@gmail.com>
2024-02-19 20:33:49 +0800

    Update logo component styling

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -5,7 +5,7 @@ import { PiNotepad } from 'react-icons/pi'
 const Logo = () => {
   return (
     <div>
-      <Link className='flex items-center' href={'/'}>
+      <Link className='flex items-center gap-1' href={'/'}>
         <PiNotepad />
         <h1 className='font-bold'>AgendaAlchemy</h1>
       </Link>
