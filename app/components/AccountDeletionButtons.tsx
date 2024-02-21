2dbd3dcb8e7938f1c1d0ae69011102203daef120
froilaaaaan1 <froilanaquino1@gmail.com>
2024-02-21 15:20:30 +0800

    Add ProfilePage and AccountDeletionButtons components

---
+++ app/components/AccountDeletionButtons.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AccountDeletionButtons = () => {
+  return (
+    <>
+      <button className='btn btn-error'>Delete Account</button>
+      <button className='btn btn-outline'>Cancel</button>
+    </>
+  )
+}
+
+export default AccountDeletionButtons
