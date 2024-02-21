86f7fd3bd5142782b9f72442d88ded2bcb8338c0
froilaaaaan1 <froilanaquino1@gmail.com>
2024-02-21 15:27:08 +0800

    fix escape character errors

--- app/app/settings/delete-account/page.tsx
+++ app/app/settings/delete-account/page.tsx
@@ -18,12 +18,12 @@ const DeleteAccountPage = () => {
 
       <div className='flex flex-col p-3 gap-14'>
         <div className='gap-3 flex flex-col' >
-          <h1 className='font-bold'>We're sorry to see you go!</h1>
+          <h1 className='font-bold'>We&apos;re sorry to see you go!</h1>
           <div className='flex-col flex gap-4'>
-            <p>We understand that you've decided to delete your account with AgendaAlchemy. Before you say goodbye, we wanted to make sure you're aware of a few things:</p>
+            <p>We understand that you&apos;ve decided to delete your account with AgendaAlchemy. Before you say goodbye, we wanted to make sure you&apos;re aware of a few things:</p>
             <ul className='gap-3 flex flex-col'>
               <li className='list-inside list-disc'>Deleting your account is permanent. Once you confirm, all your notes, tags, and other data associated with your account will be permanently deleted and cannot be recovered.</li>
-              <li className='list-inside list-disc'>Data deletion, depending on the app's privacy policy, your data may be stored for a certain period even after you delete your account. Please refer to the app's privacy policy for more information.</li>
+              <li className='list-inside list-disc'>Data deletion, depending on the app&apos;s privacy policy, your data may be stored for a certain period even after you delete your account. Please refer to the app&apos;s privacy policy for more information.</li>
             </ul>
           </div>
         </div>
@@ -32,15 +32,15 @@ const DeleteAccountPage = () => {
           <h1 className='font-bold'>Are you sure you want to delete your account?</h1>
           <div>
             <ul className='flex-col flex gap-3'>
-              <li className='list-inside list-disc'>If you're sure, click the "Delete Account" button below.</li>
-              <li className='list-inside list-disc'>If you'd like to keep your account, click "Cancel".</li>
+              <li className='list-inside list-disc'>If you&apos;re sure, click the &quot;Delete Account&quot; button below.</li>
+              <li className='list-inside list-disc'>If you&apos;d like to keep your account, click &quot;Cancel&quot;.</li>
             </ul>
           </div>
         </div>
 
         <div>
           <h1>We appreciate your feedback!</h1>
-          <p>If you have any feedback about why you're deleting your account, we'd love to hear it. Your feedback helps us improve our app for future users. You can share your feedback by sendng a message to this <a href='mailto:froilanaquino1@gmail.com'>email.</a></p>
+          <p>If you have any feedback about why you're deleting your account, we&apos;d love to hear it. Your feedback helps us improve our app for future users. You can share your feedback by sendng a message to this <a href='mailto:froilanaquino1@gmail.com'>email.</a></p>
         </div>
         
         <div className='flex flex-col gap-5'>
@@ -48,7 +48,7 @@ const DeleteAccountPage = () => {
           <div>
             <p>We wish you all the best in your future endeavors.</p>
             <p>Sincerely,</p>
-            <p>The AgendaAlchemy's Developer</p>
+            <p>The AgendaAlchemy&apos;s Developer</p>
           </div>
         </div>
 
