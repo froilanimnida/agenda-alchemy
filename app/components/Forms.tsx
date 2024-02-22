c558febd16e9dae33f542c87dd24d5a7163f2e18
froilaaaaan1 <froilanaquino1@gmail.com>
2024-02-22 16:30:47 +0800

    Merge branch 'master' of https://github.com/froilaaaaan1/agenda-alchemy

--- app/components/Forms.tsx
+++ app/components/Forms.tsx
@@ -171,32 +171,6 @@ const FormComponent = ({mode}: {mode: string}) => {
     )
   }
 
-  else if (mode === 'edit-info') {
-    return (
-      <form className='border p-5 rounded-2xl flex flex-col gap-5'>
-        <div>
-          {editInfoFields.map(field => {
-            return (
-              <label key={field.id} className="form-control w-full max-w-xs">
-                <div className="label">
-                  <span className="label-text">{field.label}</span>
-                </div>
-                <input onChange={field.onchange} ref={ field.ref } type={field.type} placeholder={field.placeholder} className="input input-bordered w-full max-w-xs" />
-              </label>
-            )
-          })}
-        </div>
-
-        <div className='flex flex-col gap-3 justify-between items-center'>
-          <button type='submit' className='btn btn-primary'>
-            Okay
-            <FaCircleArrowRight />
-          </button>
-        </div>
-      </form>
-    )
-  }
-
   else {
     return (
       <form className='border p-5 rounded-2xl' onSubmit={handleSignIn}>
